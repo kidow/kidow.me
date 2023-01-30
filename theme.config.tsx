4a6d0fb64d00cb8b241ad363874c28056700a6a6
kidow <wcgo2ling@gmail.com>
2023-01-30 14:52:37 +0900

    tailwindcss, feedbank

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,15 +4,26 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/kidow/kidow.me'
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com'
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/kidow/kidow.me',
   footer: {
-    text: 'Nextra Docs Template',
+    text: `© ${new Date().getFullYear()} kidow. All rights reserved.`
   },
+  head: (
+    <>
+      <script
+        type="text/javascript"
+        async
+        defer
+        src="https://cdn.feedbank.app/plugin.js"
+        plugin-key="53e1b21b-52a5-457c-8209-e4f8f12667ff"
+      />
+    </>
+  )
 }
 
 export default config
