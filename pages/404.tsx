85698055ba2aa4352c7e25ca1f38e4ce12b99158
kidow <wcgo2ling@gmail.com>
2023-01-30 15:20:36 +0900

    resume

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+interface State {}
+
+const NotFoundPage: NextPage = () => {
+  const { replace } = useRouter()
+
+  useEffect(() => {
+    replace('/')
+  }, [])
+  return <></>
+}
+
+export default NotFoundPage
