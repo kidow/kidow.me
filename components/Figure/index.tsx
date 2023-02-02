24ad9cc9d64bbda6bdc4cf2dad37824831b7c031
kidow <wcgo2ling@gmail.com>
2023-02-02 22:00:11 +0900

    유지보수가 쉬운 리액트 코드 짜는 법 - (1) 프로젝트 설계

--- components/Figure/index.tsx
+++ components/Figure/index.tsx
@@ -10,14 +10,14 @@ interface State {}
 
 const Figure: FC<Props> = ({ caption, image }) => {
   return (
-    <p className="nx-mt-6 nx-leading-7 first:nx-mt-0">
+    <div className="nx-mt-6 nx-leading-7 first:nx-mt-0">
       <figure>
-        <img src={image} alt={caption} />
+        <img src={image} alt={caption} className="mx-auto" />
         <figcaption className="mt-2 text-center text-xs text-neutral-500 dark:text-neutral-400 md:mt-3 md:text-sm">
           {caption}
         </figcaption>
       </figure>
-    </p>
+    </div>
   )
 }
 
