2fa35415c994270d3dfbc945e62d430ac4865db3
kidow <wcgo2ling@gmail.com>
2023-02-03 09:12:39 +0900

    Contribution spinner

--- containers/Contribution/index.tsx
+++ containers/Contribution/index.tsx
@@ -4,17 +4,20 @@ import ActivityCalendar from 'react-activity-calendar'
 import type { Day } from 'react-activity-calendar'
 import ReactTooltip from 'react-tooltip'
 import { useObjectState } from 'services'
+import { Spinner } from 'components'
 
 export interface Props {}
 interface State {
   theme: string
   list: Day[]
+  isLoading: boolean
 }
 
 const Contribution: FC<Props> = () => {
-  const [{ list, theme }, setState] = useObjectState<State>({
+  const [{ list, theme, isLoading }, setState] = useObjectState<State>({
     theme: '',
-    list: []
+    list: [],
+    isLoading: true
   })
 
   useEffect(() => {
@@ -23,47 +26,58 @@ const Contribution: FC<Props> = () => {
       .then((json) =>
         setState({
           list: json?.contributions || [],
-          theme: document.documentElement.style.getPropertyValue('color-scheme')
+          theme:
+            document.documentElement.style.getPropertyValue('color-scheme'),
+          isLoading: false
         })
       )
-      .catch((err) => console.error(err))
+      .catch((err) => {
+        console.error(err)
+        setState({ isLoading: false })
+      })
   }, [])
   return (
     <div className="mt-16">
-      <ActivityCalendar
-        data={list}
-        labels={{
-          months: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-            'Oct',
-            'Nov',
-            'Dec'
-          ],
-          weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-          totalCount: '{{count}} contributions in {{year}}',
-          tooltip: '<strong>{{count}} contributions</strong> on {{date}}',
-          legend: {
-            less: 'Less',
-            more: 'More'
-          }
-        }}
-        theme={{
-          level0: theme === 'dark' ? '#161b22' : '#ebedf0',
-          level1: theme === 'dark' ? '#0e4429' : '#9be9a8',
-          level2: theme === 'dark' ? '#006d32' : '#40c463',
-          level3: theme === 'dark' ? '#26a641' : '#30a14e',
-          level4: theme === 'dark' ? '#39d353' : '#216e39'
-        }}
-        children={<ReactTooltip html />}
-      />
+      {isLoading ? (
+        <div className="flex h-40 items-center justify-center">
+          <Spinner className="h-5 w-5" />
+        </div>
+      ) : (
+        <ActivityCalendar
+          data={list}
+          labels={{
+            months: [
+              'Jan',
+              'Feb',
+              'Mar',
+              'Apr',
+              'May',
+              'Jun',
+              'Jul',
+              'Aug',
+              'Sep',
+              'Oct',
+              'Nov',
+              'Dec'
+            ],
+            weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            totalCount: '{{count}} contributions in {{year}}',
+            tooltip: '<strong>{{count}} contributions</strong> on {{date}}',
+            legend: {
+              less: 'Less',
+              more: 'More'
+            }
+          }}
+          theme={{
+            level0: theme === 'dark' ? '#161b22' : '#ebedf0',
+            level1: theme === 'dark' ? '#0e4429' : '#9be9a8',
+            level2: theme === 'dark' ? '#006d32' : '#40c463',
+            level3: theme === 'dark' ? '#26a641' : '#30a14e',
+            level4: theme === 'dark' ? '#39d353' : '#216e39'
+          }}
+          children={<ReactTooltip html />}
+        />
+      )}
     </div>
   )
 }
