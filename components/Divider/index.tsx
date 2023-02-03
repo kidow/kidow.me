9d7b6c78c667aba649dbc135624e632527eba724
kidow <wcgo2ling@gmail.com>
2023-02-03 16:12:56 +0900

    Divider

--- components/Divider/index.tsx
+++ components/Divider/index.tsx
@@ -6,8 +6,11 @@ export interface Props {
 interface State {}
 
 const Divider: FC<Props> = ({ mode = 'horizontal' }) => {
-  if (mode === 'vertical') return <div className="w-px bg-neutral-200" />
-  return <hr className="my-8" />
+  if (mode === 'vertical')
+    return (
+      <hr className="mx-8 h-full min-h-[20px] w-px border-l border-neutral-200 dark:border-neutral-700" />
+    )
+  return <hr className="my-8 h-px border-none dark:bg-neutral-700" />
 }
 
 export default Divider
