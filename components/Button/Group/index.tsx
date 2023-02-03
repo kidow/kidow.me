70a36267b54f6a74a238ac669975187ce74054a7
kidow <wcgo2ling@gmail.com>
2023-02-04 01:15:56 +0900

    ButtonGroup

--- components/Button/Group/index.tsx
+++ components/Button/Group/index.tsx
@@ -7,10 +7,10 @@ export interface Props {
 
 const ButtonGroup: FC<Props> = ({ list, onClick }) => {
   return (
-    <div className="inline-flex divide-x rounded border bg-white">
+    <div className="inline-flex divide-x rounded border bg-white dark:divide-neutral-700 dark:border-neutral-700 dark:bg-neutral-800">
       {list.map((name, key) => (
         <button
-          className="py-2 px-3 text-sm text-neutral-400 hover:bg-sky-50 hover:text-blue-500"
+          className="py-2 px-3 text-sm text-neutral-400 first:rounded-l last:rounded-r hover:bg-amber-50 hover:text-primary dark:hover:bg-amber-200/30"
           key={key}
           onClick={() => onClick(key)}
         >
