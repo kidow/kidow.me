4df1330871ff0e76ab7eab873607093768e382bf
kidow <wcgo2ling@gmail.com>
2023-02-04 00:44:29 +0900

    Progress

--- components/Progress/index.tsx
+++ components/Progress/index.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 const Progress: FC<Props> = ({ value }) => {
   return (
-    <div className="h-3 rounded-full border">
+    <div className="h-3 rounded-full border dark:border-neutral-800">
       <div
-        className="h-full animate-slide rounded-full bg-sky-400 bg-[length:30px_30px]"
+        className="h-full animate-slide rounded-full bg-primary bg-[length:30px_30px]"
         style={{
           backgroundImage:
             'linear-gradient(135deg, rgba(255, 255, 255, .15) 25%, transparent 25%, transparent 50%, rgba(255, 255, 255, .15) 50%, rgba(255, 255, 255, .15) 75%, transparent 75%, transparent)',
