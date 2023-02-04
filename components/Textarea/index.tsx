7aff1976574e90c12eda1a591aed8fa43387ea2e
kidow <wcgo2ling@gmail.com>
2023-02-04 10:44:38 +0900

    Textarea

--- components/Textarea/index.tsx
+++ components/Textarea/index.tsx
@@ -46,8 +46,10 @@ const Textarea: FC<Props> = ({
         id={props.id || id}
         onKeyDown={onKeyDown}
         className={classnames(
-          'rounded border bg-white p-2 read-only:cursor-default focus:outline-none disabled:cursor-not-allowed',
-          !!error ? 'border-red-500' : 'border-neutral-300',
+          'rounded border bg-white p-2 read-only:cursor-default focus:outline-none disabled:cursor-not-allowed dark:bg-neutral-800',
+          !!error
+            ? 'border-red-500'
+            : 'border-neutral-300 dark:border-neutral-700',
           {
             'w-full': fullWidth,
             'bg-neutral-100': props.disabled,
@@ -61,7 +63,7 @@ const Textarea: FC<Props> = ({
         <label
           htmlFor={props.id || id}
           className={classnames(
-            'absolute -top-6 left-0 cursor-text select-none truncate text-sm text-neutral-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-neutral-400 peer-empty:left-3 peer-focus:left-0 peer-focus:-top-6 peer-focus:w-full peer-focus:cursor-default peer-focus:text-sm peer-focus:text-neutral-600',
+            'absolute -top-6 left-0 cursor-text select-none truncate text-sm text-neutral-600 transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-neutral-400 peer-empty:left-3 peer-focus:left-0 peer-focus:-top-6 peer-focus:w-full peer-focus:cursor-default peer-focus:text-sm peer-focus:text-neutral-600 dark:text-neutral-400 dark:peer-focus:text-neutral-400',
             !!props.value ? 'left-0' : 'left-3'
           )}
         >
