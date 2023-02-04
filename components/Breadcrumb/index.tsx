a96e77e4b08bfdac54e91fda7c36bdb75140e645
kidow <wcgo2ling@gmail.com>
2023-02-04 11:07:16 +0900

    Breadcrumb

--- components/Breadcrumb/index.tsx
+++ components/Breadcrumb/index.tsx
@@ -14,20 +14,26 @@ const Breadcrumbs: FC<Props> = ({ list }) => {
       <ul className="flex items-center">
         {list.map((item, key) => (
           <li
-            className={classnames('flex items-center', {
-              'before:ml-2 before:mr-3 before:block before:h-1.5 before:w-1.5 before:rotate-45 before:border-t before:border-r before:border-black before:bg-transparent before:opacity-40 before:content-[""]':
+            className={classnames('flex items-center dark:text-neutral-500', {
+              'before:ml-2 before:mr-3 before:block before:h-1.5 before:w-1.5 before:rotate-45 before:border-t before:border-r before:border-black before:bg-transparent before:opacity-40 before:content-[""] dark:before:border-neutral-400':
                 key !== 0
             })}
             key={key}
           >
-            <a
-              className={classnames(
-                key === list.length - 1 ? 'cursor-text' : 'hover:underline'
-              )}
-              href={item.path}
-            >
-              {item.name}
-            </a>
+            {list.length === key + 1 ? (
+              <span className="font-semibold dark:text-neutral-400">
+                {item.name}
+              </span>
+            ) : (
+              <a
+                className={classnames(
+                  key === list.length - 1 ? 'cursor-text' : 'hover:underline'
+                )}
+                href={item.path}
+              >
+                {item.name}
+              </a>
+            )}
           </li>
         ))}
       </ul>
